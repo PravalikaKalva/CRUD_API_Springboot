704106527158cf0e27a3c02b4fe1264299b13cf4
Pravalika <kalva.pravalika987@gmail.com>
2023-03-16 12:09:15 +0530

    Initial commit

---
+++ src/main/java/com/webla/clinical/service/PatientService.java
@@ -0,0 +1,15 @@
+package com.webla.clinical.service;
+
+import java.util.List;
+
+
+import com.webla.clinical.dto.PatientDto;
+
+public interface  PatientService{
+	public List<PatientDto> getAllPatients();
+	public int savePatient(PatientDto patientDto);
+	public PatientDto getPatient(int id);
+	public String deleteById(int id);
+	public String updatePatient(PatientDto patientDto);
+
+}
